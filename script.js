7bc400e02d3c70c7c378dd3170e07c7184b8bb50
MaisEnaya <me880027@gmail.com>
2023-09-30 16:59:54 -0400

    W

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
+let fun = 300;
 const content = document.getElementById("content");
-let sparkle = document.getElementById("sparkle");
-let path = sparkle.querySelector("path");
-let fun;
 
 content.addEventListener("mousemove", function (event) {
     const containerRect = content.getBoundingClientRect();
     const mouseX = event.clientX - containerRect.left;
 
     // Calculate the hue based on the mouse position
-    fun = (mouseX / containerRect.width) * 360;
+    // fun = (mouseX / containerRect.width) * 300;
+    fun = Math.min(Math.max((mouseX / containerRect.width) * 140, 0), 140) + 160;
 
-    // Set the value of the --hue CSS variable
-    document.documentElement.style.setProperty('--fun', fun);
-    path.style.fill = `hsl(${fun}, 100%, 90%)`;
+
+    // Update the CSS variable by adding 'deg' to the value
+    document.documentElement.style.setProperty("--fun", `${fun}deg`);
 });
-// sparkle.querySelector("path")
-
