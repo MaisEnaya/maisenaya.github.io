9018651a8ff65c67f7c7276dd719df844652294a
Mais <47697188+MaisEnaya@users.noreply.github.com>
2023-10-13 13:53:41 +0300

    nav bar update

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ content.addEventListener("mousemove", function (event) {
     // Update the CSS variable by adding 'deg' to the value
     document.documentElement.style.setProperty("--fun", `${fun}deg`);
 });
+
+
+
+// nav script 
+//  navigation adapted from a11ymaaters, Accessible Mobile Navigation publihsed on Sunday, September 18, 2016 by Ahmad Shadeed 
+
+var toggle = document.querySelector('#toggle');
+var menu = document.querySelector('#menu');
+var menuItems = document.querySelectorAll('#menu li a');
+
+// toggle.style.setAttribute('area-expanded','false');
+
+toggle.addEventListener('click', function(){
+  if (menu.classList.contains('is-active')) {
+    this.setAttribute('aria-expanded', 'false');
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active'); 
+    this.setAttribute('aria-expanded', 'true');
+    //menuItems[0].focus();
+  }
+});
+
+// end of nav script
